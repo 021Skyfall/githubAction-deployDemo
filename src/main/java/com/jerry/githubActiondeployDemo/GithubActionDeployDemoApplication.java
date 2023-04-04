fe11666b61a07d4b77d3a49626b30ba88250fdf0
021Skyfall <021skyfall@gmail.com>
2023-04-04 13:02:13 +0900

    githubAction 연승용도로 업로드

---
+++ src/main/java/com/jerry/githubActiondeployDemo/GithubActionDeployDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jerry.githubActiondeployDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionDeployDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionDeployDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jerry/githubActiondeployDemo/GithubActionDeployDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jerry.githubActiondeployDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionDeployDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
